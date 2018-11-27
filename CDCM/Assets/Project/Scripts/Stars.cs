566771f01aa877be608bb355c96bc68183507f48
grant <gnafzig@iastate.edu>
2018-11-26 18:31:10 -0600

    CDCM Unity Project

---
+++ CDCM/Assets/Project/Scripts/Stars.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour {
+    
+    //modified code from this video: https://www.youtube.com/watch?v=YuPEmRXtwIg
+    private Transform thisTrans;
+    private ParticleSystem.Particle[] stars;
+    private float starDistSqr;
+    private float starClipDistSqr;
+
+    public Color starColor;
+    public int maxStars = 600;
+    public float starSize = .35f;
+    public float distanceFromUser = 50f;
+    public float cameraClipDistance = 15f;
+
+
+	// Use this for initialization
+	void Start () {
+        thisTrans = GetComponent<Transform>();
+        starDistSqr = distanceFromUser * distanceFromUser;
+        starClipDistSqr = cameraClipDistance * cameraClipDistance;
+
+	}
+	
+    private void CreateParticles()
+    {
+        stars = new ParticleSystem.Particle[maxStars];
+
+        for(int i = 0; i < maxStars; i++)
+        {
+            stars[i].position = Random.insideUnitSphere * distanceFromUser + thisTrans.position;
+            stars[i].color = new Color(starColor.r, starColor.g, starColor.b, starColor.a);
+            stars[i].size = starSize;
+
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+		if(stars == null)
+        {
+            CreateParticles();
+        }
+
+        for (int i = 0; i < maxStars; i++)
+        {
+            if((stars[i].position - thisTrans.position).sqrMagnitude > starDistSqr)
+            {
+                stars[i].position = Random.insideUnitSphere.normalized * distanceFromUser + thisTrans.position;
+            }
+            if ((stars[i].position - thisTrans.position).sqrMagnitude <= starClipDistSqr)
+            {
+                float percent = (stars[i].position - thisTrans.position).sqrMagnitude / starClipDistSqr;
+                stars[i].color = new Color(1, 1, 1, percent);
+                stars[i].size = percent * starSize;
+            }
+        }
+        GetComponent<ParticleSystem>().SetParticles(stars, stars.Length);
+
+    }
+}
---
+++ CDCM/Assets/Project/Scripts/UserController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserController : MonoBehaviour {
+    //Transform object used to change user's position/view
+    public Transform viewT;
+    //Force modifiers for force and torque
+    public float forceMod = 5, torqueMod = 0.5f;
+    //Vector3 objects representing values for torque & force
+    public Vector3 forceVal, torqueVal;
+
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        rb.angularVelocity = Vector3.zero;
+    }
+    private void FixedUpdate()
+    {
+        //force & torque initializations
+        forceVal = new Vector3(0, 0, 0);
+        torqueVal = new Vector3(0, 0, 0);
+
+                //force calculations:
+        //forward propulsion
+        if (Input.GetKey("w"))
+        {
+            forceVal += viewT.forward * forceMod;
+        }
+        //left propulsion
+        if (Input.GetKey("a"))
+        {
+            forceVal += -viewT.right * forceMod;
+        }
+        //backwards propulsion
+        if (Input.GetKey("s"))
+        {
+            forceVal += -viewT.forward * forceMod;
+        }
+        //right propulsion
+        if (Input.GetKey("d"))
+        {
+            forceVal += viewT.right * forceMod;
+        }
+        //upwards propulsion
+        if (Input.GetKey("space"))
+        {
+            forceVal += viewT.up * forceMod;
+        }
+        //downwards propulsion
+        if (Input.GetKey("c"))
+        {
+            forceVal += -viewT.up * forceMod;
+        }
+
+                    //torque/rotation calcs
+        //roll left
+        if (Input.GetKey("q"))
+        {
+            torqueVal += viewT.forward * torqueMod;
+        }
+        //roll right
+        if (Input.GetKey("e"))
+        {
+            torqueVal += -viewT.forward * torqueMod;
+        }
+        //yaw right
+        if (Input.GetKey("right"))
+        {
+            torqueVal += viewT.up * torqueMod;
+        }
+        //yaw left
+        if (Input.GetKey("left"))
+        {
+            torqueVal += -viewT.up * torqueMod;
+        }
+        //pitch forwards (down)
+        if (Input.GetKey("up"))
+        {
+            torqueVal += viewT.right * torqueMod;
+        }
+        //pitch backwards (up)
+        if (Input.GetKey("down"))
+        {
+            torqueVal += -viewT.right * torqueMod;
+        }
+        //stabilization
+        if (Input.GetKey("z"))
+        {
+            forceVal = -rb.velocity * forceMod * 0.3f;
+            //torqueVal = -rb.angularVelocity.normalized * torqueMod * 1f;
+            if (rb.angularVelocity.magnitude > 2f * Time.deltaTime)
+            {
+               rb.AddTorque(-(rb.angularVelocity/4) * torqueMod, ForceMode.Force);
+            }
+            else
+            {
+                rb.angularVelocity = Vector3.zero;
+            }
+
+            Debug.Log("Stabilizing User");
+        }
+
+        //apply force
+        rb.AddForce(forceVal);
+        //apply torque
+        rb.AddTorque(torqueVal);
+    }
+
+
+}
